6e2311657f292ca9c7a71673a37582e231169d78
Alex Creasy <acreasy@redhat.com>
2021-08-06 22:21:44 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,13 @@
 !/usr/bin/env bash
 
+cp .{path,bash_profile,bash_prompt,exports,aliases,functions,extra} ${HOME} 2>/dev/null
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update && brew bundle install
+
+./macos-config.sh
+
 sudo -v
 
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain alcr.ca.pem
-
-cp ".{path,bash_prompt,exports,aliases,functions,extra}" "${HOME}"
